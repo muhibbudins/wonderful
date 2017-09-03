d84b2df16bfa916ab38c24f09bde4d8884b4fc55
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2017-09-03 09:18:51 +0700

    add toolbar, redesign table, card, button and list post

--- js/default.js
+++ js/default.js
@@ -1,2 +1,11 @@
 $(document).ready(function() {
+
+	// Auto add Icon at card
+	$cbIcon = $('.card-icon-background');
+	$cbIcon.map(function(k,v) {
+		_self = $(v),
+		_icon = _self.find('.card-title i').clone();
+		
+		_self.find('.card-content').append($(_icon[0]).addClass('card-content-icon'));
+	});
 });
